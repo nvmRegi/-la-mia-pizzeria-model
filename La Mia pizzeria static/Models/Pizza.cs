4dee73f951d325f50de913095f6f0e24dc4819b9
regi <cabael.regine@gmail.com>
2022-05-10 11:35:42 +0200

    Add project files.

---
+++ La Mia pizzeria static/Models/Pizza.cs
@@ -0,0 +1,12 @@
+﻿namespace La_Mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Nome { get; set; }
+        public string Ingrediente { get; set; }
+        public string Image { get; set; }
+        public double Prezzo { get; set; }
+
+        public Pizza() { }
+    }
+}
---
+++ La Mia pizzeria static/Utils/MenùData.cs
@@ -0,0 +1,26 @@
+﻿using La_Mia_pizzeria_static.Models;
+
+namespace La_Mia_pizzeria_static.Utils
+{
+    public static class MenùData
+    {
+        private static List<Pizza> pizze;
+
+        public static List<Pizza> GetPizze()
+        {
+            if(MenùData.pizze != null)
+            {
+                return MenùData.pizze;
+            }
+
+            List<Pizza> nuovaListaPizza = new List<Pizza>();
+
+            for(int i = 0; i < 5; i++)
+            {
+                Pizza pizza = new Pizza();
+            }
+
+
+        }
+    }
+}
